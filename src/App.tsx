8dbad6e42557634ce87850bcdfc71bb007394162
Vinícius Di Santi Cascales Aragao <vinicius@MacBook-Air-de-Vinicius.local>
2022-01-16 14:58:00 -0300

    feat: criando modal de adicionar novas transactions

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { createServer } from 'miragejs';
+import Modal from 'react-modal';
+import { useState } from "react";
+
+Modal.setAppElement('#root');
 
 createServer({
   routes() {
@@ -22,10 +26,29 @@ createServer({
 });
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
+    
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+      >
+          <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
